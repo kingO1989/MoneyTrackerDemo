97d33deca2718dc5049d5a36b1aa1972b6fd2fb0
abdulsaheed <kingolaribigbe@gmail.com>
2024-02-11 10:32:50 -0500

    Updates

--- static/js/72.f81c69ec.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmoneytracker=self.webpackChunkmoneytracker||[]).push([[72],{72:function(e,n,c){c.r(n);n.default=function(){}}}]);
-//# sourceMappingURL=72.f81c69ec.chunk.js.map
--- static/js/881.49c421f1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmoneytracker=self.webpackChunkmoneytracker||[]).push([[881],{881:function(e,n,c){c.r(n);n.default=function(){}}}]);
-//# sourceMappingURL=881.49c421f1.chunk.js.map
